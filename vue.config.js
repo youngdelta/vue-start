18247cf1d15e5a4eb5e06919835aa1541c6e392e
islandboy <youngdelta@gmail.com>
2022-09-04 15:22:31 +0900

    todo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions:{
+      scss:{
+        prependData: `
+          @import "@/assets/style/_mixins.scss";
+          @import "@/assets/style/_variables.scss";
+        `
+      }
+    }
+  }
 })
