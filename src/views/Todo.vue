d8a2d6068223666ee05bb197fb8c1bcbed713d60
islandboy <youngdelta@gmail.com>
2022-09-04 14:37:31 +0900

    todo

---
+++ src/views/Todo.vue
@@ -0,0 +1,20 @@
+<template lang="ko">
+    <div>
+        <h1>To DO list</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Todo',
+    data () {
+        return {
+
+        }
+    },methods: {
+        //
+    }
+}
+</script>
+<style lang="ko">
+    
+</style>
