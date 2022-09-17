2e2aaf8f3f6b62c39cceddd096f121a9a7bea6d0
islandboy <youngdelta@gmail.com>
2022-09-17 13:15:28 +0900

    w2

---
+++ src/components/common/Modal.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <hr>
+        <h1>무궁화 꽃이 피었습니다.</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
