4332710f6d1de956de2b15d87a53760074e4bc09
islandboy <youngdelta@gmail.com>
2022-09-17 13:21:59 +0900

    w-001

--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -2,10 +2,22 @@
     <div>
         <hr>
         <h1>무궁화 꽃이 피었습니다.</h1>
+        <div>
+          <h2>{{name}}</h2>
+        </div>
     </div>
 </template>
 <script>
 export default {
+
+  data() {
+    return {
+      name : '무궁화'
+    }
+  },
+  methods:{
+
+  }
     
 }
 </script>
