8c8a651674569206ce1e3aad3cd216d4e4c5023f
islandboy <youngdelta@gmail.com>
2022-09-17 17:00:36 +0900

    w02

--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div>
-        <hr>
+        <>
         <h1>무궁화 꽃이 피었습니다.</h1>
         <div>
             가족 행복 사람
@@ -14,6 +14,7 @@
         <div>
           <span>test</span>
         </div>
+        <hr>
     </div>
 </template>
 <script>
